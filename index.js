d18696360af949b375cb3cf407c87633dd6c5257
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 20:54:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-function Scooter() {
+function Scooter(year, color, model) {
 
 }
 
-function Driver() {
+function Driver(name, age, experience) {
 
 }
 
-function PickupLocation() {
+function PickupLocation(address, city) {
   
 }
