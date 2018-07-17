61785db9ceec64e3de8d240bbf7261689e7b72cc
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 20:55:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ function Scooter(year, color, model) {
 }
 
 function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
 
 }
 
 function PickupLocation(address, city) {
-
+  this.address = address;
+  this.city = city;
 }
